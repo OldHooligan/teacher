31b20ef126b631bf05a10960f355d462c33be4ff
xu-zhiguo <50972704+xu-zhiguo@users.noreply.github.com>
2021-10-05 13:49:30 +0800

    Update downLoadExcel.py

--- teacherDate/DB/downLoadExcel.py
+++ teacherDate/DB/downLoadExcel.py
@@ -21,6 +21,7 @@ class create_excel():
 
     def write_excel_xls(self, path):
         shutil.rmtree(f'{current_work_dir}/{self.uid}')  #递归删除非空文件夹
+        os.makedirs(f'{current_work_dir}/{self.uid}')
         index = len(self.value_title)  # 获取需要写入数据的行数
         workbook = xlwt.Workbook()  # 新建一个工作簿
         style, style2 = self.get_style()
